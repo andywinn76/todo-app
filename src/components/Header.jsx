875180e835acc79105e5e38c5377104c9dba2e70
Andy Winn <andywinn76@gmail.com>
2025-08-13 12:55:59 -0600

    Adjust header even more

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,13 +35,13 @@ export default function Header({ children }) {
   return (
     <header className="flex items-center justify-between bg-gray-100 p-4 border-b">
       <Logo />
-      <div className="flex items-center gap-4">
-        <div className="sm:w-9 sm:h-9 w-10 h-10 rounded-full bg-blue-600 text-white flex items-center justify-center font-bold text-lg">
+      <div className="flex items-center gap-4 sm:gap-2">
+        <div className="sm:w-6 sm:h-6 w-10 h-10 rounded-full bg-blue-600 text-white flex items-center justify-center font-bold text-lg">
           {initials}
         </div>
-        <h1 className="text-base md:text-lg lg:text-xl font-semibold mr-5 ">Welcome, {firstName}!</h1>
+        <h1 className="text-base md:text-lg lg:text-xl font-semibold mr-5 sm:mr-2">Welcome, {firstName}!</h1>
         <button onClick={handleLogout}>
-          <LogOut className="w-6 h-6 mr-4" />
+          <LogOut className="w-6 h-6 mr-4 sm:mr-1" />
         </button>
       </div>
 
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,13 @@
 function Logo() {
   return (
     <div>
-      <img src="/letsdoooitlogo.png" alt="Logo" className="w-10 h-10 md:w-15 md:h-15 lg:w-20 lg:h-20 ml-4" />
+      <img
+        src="/letsdoooitlogo.png"
+        alt="Logo"
+        className="w-15 h-15 md:w-20 md:h-20 lg:w-25 lg:h-25 ml-4 sm:ml-2"
+      />
     </div>
-  )
+  );
 }
 
-export default Logo
+export default Logo;
